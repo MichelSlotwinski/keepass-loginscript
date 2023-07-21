de635252c978ae990b1388b3b6cb18cc768530b5
Michel Slotwinski <114214373+MichelSlotwinski@users.noreply.github.com>
2023-07-21 16:12:28 +0200

    init

---
+++ keepassstartup.ps1
@@ -0,0 +1,22 @@
+﻿Start-Process -FilePath "C:\Program Files\Mozilla Firefox\firefox.exe" -ArgumentList "-new-window https://trusted.bedag.ch/"
+Start-Sleep -Seconds 10
+$wshell = New-Object -ComObject wscript.shell;
+
+$wshell.AppActivate('Trusted SSLVPN für uBWP')
+$wshell.SendKeys('rbdk{TAB}P@$$w0rd{TAB}P!N154829')
+
+
+KPScript -c:GetEntryString "x:\keemppriv.kdbx" -pw:d1ad0kh1wdUudH1vJfmA6 -Field:Password -ref-Title:"Remote trusted - https://trusted.bedag.ch/ (FireFox)" -Spr
+KPScript -c:GetEntryString "x:\keemppriv.kdbx" -pw:d1ad0kh1wdUudH1vJfmA6 -Field:pin -ref-Title:"Remote trusted - https://trusted.bedag.ch/ (FireFox)" -Spr
+KPScript -c:ListGroups "c:\Data\PSAutomation\keemppriv-bedag.kdbx" -pw:P@$$w0rd
+
+"KPScript.exe" "-c:GetEntryString" "MyDatabase.kdbx" "-pw:MyPassword" "-keyfile:C:\MyDatabase.key" "-refx-group:mygroup" "-ref-Title:My Entry" "-Field:URL"
+
+
+"C:\Program Files\KeePass Password Safe 2\KPScript.exe" "-c:GetEntryString" "MyDatabase.kdbx" "-pw:MyPassword" "-keyfile:C:\MyDatabase.key" "-refx-group:mygroup" "-ref-Title:My Entry" "-Field:URL"
+
+set-location
+
+Invoke-Expression -command 'KPScript.exe -c:GetEntryString "x:\keemppriv.kdbx" -pw:dP@$$w0rd -Field:UserName -ref-Title:"Remote trusted - https://trusted.bedag.ch/ (FireFox)" -Spr'
+
+"KPScript.exe" "-c:GetEntryString" "MyDatabase.kdbx" "-pw:MyPassword" "-keyfile:C:\MyDatabase.key" "-refx-group:mygroup" "-ref-Title:My Entry" "-Field:URL"
---
+++ loginscriptgui.ps1
@@ -0,0 +1,45 @@
+﻿Add-Type -AssemblyName System.Windows.Forms
+[System.Windows.Forms.Application]::EnableVisualStyles()
+
+$mslloginscriptgui = New-Object system.Windows.Forms.Form
+$mslloginscriptgui.ClientSize = New-Object System.Drawing.Point(400,300)
+$mslloginscriptgui.text = "MSL Login Script"
+$mslloginscriptgui.TopMost = $false
+
+$KeePassPW = New-Object system.Windows.Forms.MaskedTextBox
+$KeePassPW.multiline = $false
+$KeePassPW.text = "KeePassPW"
+$KeePassPW.width = 200
+$KeePassPW.height = 50
+$KeePassPW.visible = $true
+$KeePassPW.enabled = $true
+$KeePassPW.location = New-Object System.Drawing.Point(100,50)
+$KeePassPW.Font = New-Object System.Drawing.Font('Microsoft Sans Serif',10)
+$KeePassPW.ForeColor = [System.Drawing.ColorTranslator]::FromHtml("")
+
+$work = New-Object system.Windows.Forms.Button
+$work.text = "work"
+$work.width = 200
+$work.height = 50
+$work.location = New-Object System.Drawing.Point(100,125)
+$work.Font = New-Object System.Drawing.Font('Microsoft Sans Serif',10)
+
+$privat = New-Object system.Windows.Forms.Button
+$privat.text = "privat"
+$privat.width = 200
+$privat.height = 50
+$privat.location = New-Object System.Drawing.Point(100,200)
+$privat.Font = New-Object System.Drawing.Font('Microsoft Sans Serif',10)
+
+
+
+$mslloginscriptgui.controls.AddRange(@($work,$privat,$KeePassPW))
+
+$work.Add_Click({Write-Host $KeePassPW.Text
+$mslloginscriptgui.Close()})
+
+
+[void]$mslloginscriptgui.ShowDialog()
+
+
+
